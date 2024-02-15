8cbe7f3c3206f121067d4e899b7c64fff6115fbe
Nsmith4789 <156531219+Nsmith4789@users.noreply.github.com>
2024-02-14 21:24:26 -0600

    updatereadme

--- scritp.js
+++ scritp.js
@@ -1,3 +1,9 @@
 BookNow.onclick = function () {
     alert("Bam Bam")
+    BookNow.style.color = "blue";
+    BookNow.style.padding = "10px"
+}
+CancelSubscription.onclick = function () {
+    alert("POW")
+    CancelSubscription.style.color = "red";
 }
---
+++ styledbuttons.js
@@ -0,0 +1,7 @@
+teams1.onclick = function () {
+    alert("NFL.COM")
+    teams1.style.padding = "10px"
+}
+teams2.onclick = function () {
+    alert("YEEEAAAAHHHH!!!")
+}
